10ec30e53e479ea20b0f228302f03785c2a826f7
Vojtech Horky <vojtech.horky@gmail.com>
2024-02-04 19:46:34 +0100

    Generator script bits

--- boardgames/battle-of-midway/battle-of-midway-2d.sh
+++ boardgames/battle-of-midway/battle-of-midway-2d.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-generate() {
-    echo "$( date '+[%Y-%m-%d %H:%M:%S] >>>> >>> >> >' )" "Generating bom-$2.stl (mode $1) ..." >&2
-    openscad -o "bom-$2.stl" -D "to_print=$1" -D '$fn=100' battle-of-midway-2d.scad
+msg() {
+    echo "$( date '+[%Y-%m-%d %H:%M:%S] >>>> >>> >> >' )" "$@" >&2
 }
+
+generate() {
+    msg "Generating bom-v1-$2.stl (mode $1) ..." >&2
+    openscad -o "bom-v1-$2.stl" -D "to_print=$1" -D '$fn=100' battle-of-midway-2d.scad
+}
+
+start_time="$( date '+%s' )"
 
 generate 0 "fighter-generic"
 generate 1 "bomber-generic"
@@ -13,3 +19,5 @@ generate 4 "fighter-japanese"
 generate 5 "bomber-japanese"
 generate 6 "activation-ring"
 
+end_time="$( date '+%s' )"
+msg "Completed in" "$(( end_time - start_time ))" "seconds."
